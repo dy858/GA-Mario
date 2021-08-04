2c949eae68ac5d0b8ae08c99c00181f703e6c41b
LDY <74583481+dy858@users.noreply.github.com>
2021-08-04 21:55:18 +0900

    0804

--- lab_mario/03. get_screen.py
+++ lab_mario/03. get_screen.py
@@ -8,8 +8,7 @@ from PyQt5.QtCore import QTimer
 
 
 
-env = retro.make(game='SuperMarioBros-Nes', state='Level1-1')
-env.reset()
+
 
 
 #print(screen.shape[0], screen.shape[1])
@@ -20,31 +19,37 @@ class MyApp(QWidget):
         super().__init__()
         a = 2
 
-        
+        self.press_buttons = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-        screen = self.env.get_screen()
+        self.env = retro.make(game='SuperMarioBros-Nes', state='Level1-1')
+        self.env.reset()
+
+        self.screen = self.env.get_screen()
 
         #창크기 조절
-        self.setFixedSize(int(screen.shape[0] * a), int(a * screen.shape[1]))
+        self.setFixedSize(int(self.screen.shape[0] * a), int(a * self.screen.shape[1]))
         #창제목 설정
         self.setWindowTitle('GA Mario')
+
+        self.label_image = QLabel(self)
+
+        self.image = self.env.get_screen()
+        qimage = QImage(self.image, self.image.shape[1], self.image.shape[0], QImage.Format_RGB888)
+        pixmap = QPixmap(qimage)
+        pixmap = pixmap.scaled(int(self.screen.shape[0] * a), int(a * self.screen.shape[1]), Qt.IgnoreAspectRatio)
+
+        self.label_image.setPixmap(pixmap)
+        self.label_image.setGeometry(0, 0, int(self.screen.shape[0] * a), int(a * self.screen.shape[1]))
 
         self.qtimer = QTimer(self)
         # 타이머에 호출할 함수 연결
         self.qtimer.timeout.connect(self.timer)
         # 1초마다 연결된 함수를 실행
-        self.qtimer.start(1000 / 60)
+        self.qtimer.start(1000 // 60)
 
 
         #이미지
-        label_image = QLabel(self)
-        image = env.get_screen()
-        qimage = QImage(image, image.shape[1], image.shape[0], QImage.Format_RGB888)
-        pixmap = QPixmap(qimage)
-        pixmap = pixmap.scaled(int(screen.shape[0] * a), int(a * screen.shape[1]), Qt.IgnoreAspectRatio)
 
-        label_image.setPixmap(pixmap)
-        label_image.setGeometry(0, 0, int(screen.shape[0] * a), int(a * screen.shape[1]))
 
 
 
@@ -52,7 +57,51 @@ class MyApp(QWidget):
         self.show()
 
     def timer(self):
-        env.step(np.array([0, 0, 0, 1, 0, 0, 0, 0, 0]))
+        self.env.step(np.array([0, 0, 0, 0, 0, 0, 0, 0, 0]))
+
+        
+
+
+
+
+    def keyPressEvent(self, event):
+        key = event.key()
+        if key == event.key_Up:
+            self.press_buttons[4] = 1
+        elif key == event.key_Down:
+            self.press_buttons[5] = 1
+        elif key == event.key_Left:
+            self.press_buttons[6] = 1
+        elif key == event.key_Right:
+            self.press_buttons[7] = 1
+        elif key == event.key_A:
+            self.press_buttons[0] = 1
+        elif key == event.key_B:
+            self.press_buttons[8] = 1
+
+
+
+
+    def keyReleasevent(self, event):
+        key = event.key()
+        if key == event.key_Up:
+            self.press_buttons[4] = 0
+        elif key == event.key_Down:
+            self.press_buttons[5] = 0
+        elif key == event.key_Left:
+            self.press_buttons[6] = 0
+        elif key == event.key_Right:
+            self.press_buttons[7] = 0
+        elif key == event.key_A:
+            self.press_buttons[0] = 0
+        elif key == event.key_B:
+            self.press_buttons[8] = 0
+
+
+    def keyReleaseEvent(self, event):
+        key = event.key()
+        print(str(key) + 'release')
+
 
 #직접 실행할때만 실행되는 코드
 if __name__ == '__main__':
